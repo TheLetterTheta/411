686f6b6b1ff56e9e51523fc372936d8303ad5c07
Nicholas Dolan <nicholas.dolan1@gmail.com>
2016-08-24 16:12:41 -0500

    Removed Root User Password for individual change

--- Slim/app/data/const/constants.php
+++ Slim/app/data/const/constants.php
@@ -11,6 +11,6 @@ class CONSTANTS
 {
     const DB_HOST = "localhost";
     const DB_USER = "root";
-    const DB_PASSWORD = "24L;]j.,e&]nrT[WT?WA=c";
+    const DB_PASSWORD = "";
     const DB_DATABASE = "cmps_411";
 }
